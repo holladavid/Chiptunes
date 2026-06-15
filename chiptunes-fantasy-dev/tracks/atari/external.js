36637365143ce95b43e879e768945130c96c740d
holladavid <holla_david@t-online.de>
2026-06-15 20:58:05 +0200

    fix: Typo in Tracknames

--- chiptunes-fantasy-dev/tracks/atari/external.js
+++ chiptunes-fantasy-dev/tracks/atari/external.js
@@ -2,11 +2,11 @@ import { loadYmFile } from './ym-parser.js';
 
 // HIER EINFACH ALLE DATEINAMEN EINTRAGEN, DIE IN DEINEM ORDNER LIEGEN!
 const myYmFiles = [
-    "thalion_loader.ym",           // Dein erster Test-Song
-    "turrican1_world_1_1.ym",      // Lade hier deine entpackten Lieblingstracks rein...
-    "WINGLEV1.ym",               // ...
-    "GOLDRUN.ym",
-    "spherical_intro.ym"
+    "thalion_loader.YM",           // Dein erster Test-Song
+    "turrican1_world_1_1.YM",      // Lade hier deine entpackten Lieblingstracks rein...
+    "WINGLEV1.YM",               // ...
+    "GOLDRUN.YM",
+    "spherical_intro.YM"
 ];
 
 // Wir generieren mit .map() automatisch für jeden Namen einen fertigen Track-Eintrag!
